c70fd497873f042d18f508f8a5c7b2831c7afe40
SongGol <dldmsgus2@gmail.com>
2021-07-14 13:39:11 +0900

    fragment추가

--- app/src/main/java/com/example/assignment3/MainActivity.kt
+++ app/src/main/java/com/example/assignment3/MainActivity.kt
@@ -4,20 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.assignment3.databinding.ActivityMainBinding
+import com.example.assignment3.databinding.FragmentInterestBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: FragmentInterestBinding
     var stockArrayList = ArrayList<Stock>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = FragmentInterestBinding.inflate(layoutInflater)
+        //binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
         initializeDataSet()
 
-        binding.mainBtn.setOnClickListener{
+        binding.addStockBtn.setOnClickListener{
             val intent = Intent(this, InterestStockActivity::class.java)
             startActivity(intent)
         }
