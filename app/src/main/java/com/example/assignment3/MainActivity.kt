03b1c6808aae2301e19dc3f0416b7b61bd7cb732
SongGol <dldmsgus2@gmail.com>
2021-07-15 11:07:20 +0900

    약간 추가

--- app/src/main/java/com/example/assignment3/MainActivity.kt
+++ app/src/main/java/com/example/assignment3/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.assignment3
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.assignment3.databinding.ActivityMainBinding
 import com.example.assignment3.databinding.FragmentInterestBinding
 
+
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    val INTEREST = "interest"
+    val COMMUNITY = "community"
+    val TREND = "trend"
+    val DISCOVER = "discover"
+    val MORE = "more"
+    private var CUR = INTEREST
+    private var backPressedTime: Long = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,13 +33,46 @@ class MainActivity : AppCompatActivity() {
 
         supportFragmentManager.beginTransaction().apply {
             replace(binding.mainFragment.id, InterestFragment())
-            addToBackStack(null)
+            addToBackStack(INTEREST)
             commit()
         }
 
+        //onclick listener설정
+        //관심종목
+        binding.mainInterestFrgIv.setOnClickListener {
+            supportFragmentManager.popBackStack(CUR, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+            CUR = INTEREST
+
+            supportFragmentManager.beginTransaction().apply {
+                replace(binding.mainFragment.id, InterestFragment())
+                addToBackStack(CUR)
+                commit()
+            }
+        }
+
+        //더보기
+        binding.mainMoreIv.setOnClickListener {
+            supportFragmentManager.popBackStack(CUR, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+            CUR = MORE
+
+            supportFragmentManager.beginTransaction().apply {
+                replace(binding.mainFragment.id, MoreFragment())
+                addToBackStack(CUR)
+                commit()
+            }
+        }
 
 
     }
 
-
+    override fun onBackPressed() {
+        if (System.currentTimeMillis() > backPressedTime + 2000) {
+            backPressedTime = System.currentTimeMillis()
+            Toast.makeText(this, "뒤로 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show()
+        } else {
+            finish()
+            System.exit(0)
+            android.os.Process.killProcess(android.os.Process.myPid())
+        }
+    }
 }
---
+++ app/src/main/java/com/example/assignment3/MoreFragment.kt
@@ -0,0 +1,21 @@
+package com.example.assignment3
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.assignment3.databinding.FragmentMoreBinding
+
+class MoreFragment : Fragment() {
+    private lateinit var binding: FragmentMoreBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMoreBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
